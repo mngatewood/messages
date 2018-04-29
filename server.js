ec6b11126868381fddf8749c13685943fbb6759d
Michael Gatewood <michael@mngatewood.com>
2018-04-29 12:56:20 -0600

    Adds corrections to POST

--- server.js
+++ server.js
@@ -6,13 +6,30 @@ server.listen(3000, () => {
   console.log('The HTTP server is listening at Port 3000.');
 });
 
+let messages = [
+  { 'id': 1, 'user': 'brittany storoz', 'message': 'hi there!' },
+  { 'id': 2, 'user': 'bob loblaw', 'message': 'check out my law blog' },
+  { 'id': 3, 'user': 'lorem ipsum', 'message': 'dolor set amet' }
+];
+
+const getAllMessages = (response) => {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.write(JSON.stringify(messages))
+  response.end();
+}
+
+const addMessage = (newMessage, response) => {
+  messages.push(newMessage);
+  response.writeHead(201, { "Content-Type": "application/json" });
+  response.write(JSON.stringify(newMessage));
+  response.end();
+}
+
 server.on('request', (request, response) => {
   if (request.method === 'GET') {
     getAllMessages(response);
-  }
-
-  else if (request.method === 'POST') {
-    let newMessage = messages[3];
+  } else if (request.method === 'POST') {
+    let newMessage = { 'id': new Date() };
 
     request.on('data', (data) => {
       newMessage = Object.assign(newMessage, JSON.parse(data));
@@ -23,22 +40,3 @@ server.on('request', (request, response) => {
     });
   }
 });
-
-const getAllMessages = (response) => {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write(JSON.stringify(messages))
-  response.end();
-}
-
-const addMessage = (newMessage, response) => {
-  response.writeHead(201, {"Content-Type": "text/plain"});
-  response.write(JSON.stringify(newMessage));
-  response.end();
-}
-
-let messages = [
-  { 'id': 1, 'user': 'brittany storoz', 'message': 'hi there!' },
-  { 'id': 2, 'user': 'bob loblaw', 'message': 'check out my law blog' },
-  { 'id': 3, 'user': 'lorem ipsum', 'message': 'dolor set amet' },
-  { 'id': 4, 'user': 'alex trebek', 'message': 'answer in the form of a question' }
-];
